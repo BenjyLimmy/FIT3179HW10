ca82429f9389eb937fe732790201a321b0a6aa9d
BenjyLimmy <126404899+BenjyLimmy@users.noreply.github.com>
2024-09-22 21:50:15 +0800

    Upload W10 Chart

---
+++ bubble_chart.js
@@ -0,0 +1,4 @@
+var spec = "bubble_chart.json";
+vegaEmbed('#bubble_chart', spec).then(function(result) {
+// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
